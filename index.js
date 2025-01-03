79b5b8bbbe8f869bb98509cb100698825332dc8a
Susan Shrestha <susanshrestha@susan.local>
2025-01-03 10:31:42 +0545

    index page updated content

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Jenkins Training Session!");
+  res.send("Welcome to Jenkins with CI/CD pipeline Training Session!");
 });
 
 app.listen(8080, () => {
